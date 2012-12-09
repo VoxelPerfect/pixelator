f5a253a928c2ad5edca520546a0325d9d9e60bf8
VoxelPerfect <kostas.karolemeas@me.com>
2012-12-09 06:33:24 +0200

    Added license and WebStorm/PhpStorm project files

--- www/js/global.js
+++ www/js/global.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2012 Kostas Karolemeas, Spiros Xenos
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 var pixelator = {};
 
 // http://localhost/hobistic/anima/www/index.html?scale=18.0&density=2&impulse=300&gravity=9.81&damp=0.4&debug=true
